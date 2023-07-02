d230e12aa7be9bd1866ce23bec89150127c0d5c5
Haseebz1 <haseebkandore@hotmail.com>
2023-07-03 00:53:57 +0100

    npm Init for intializating and packages installed along npm inquirer@8.2.4

--- db/schema.sql
+++ db/schema.sql
@@ -2,15 +2,18 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
+
 CREATE TABLE department
 (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL
+    AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR (30)
 );
 
 CREATE TABLE role
 (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL 
+    AUTO_INCREMENT PRIMARY KEY,
     tittle VARCHAR (30),
     salary DECIMAL,
     department_id INT,
@@ -21,7 +24,8 @@ CREATE TABLE role
 
 CREATE TABLE employee 
 (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL 
+    AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR (30),
     last_name VARCHAR (30),
     role_id INT,
@@ -29,4 +33,4 @@ CREATE TABLE employee
     FOREIGN KEY (role_id)
     REFERENCES role (id)
     ON DELETE SET NULL
-)
+);
