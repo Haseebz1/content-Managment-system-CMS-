d4bc2f910f18ac331efc6abc9ec779b6a31cd5a5
Haseebz1 <haseebkandore@hotmail.com>
2023-07-03 00:37:39 +0100

    Seeds data put some default department , role and employye with insrt into statment

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO department (id, name) VALUES (1, 'Sales');
+INSERT INTO department (id, name) VALUES (2, 'Engineering');
+INSERT INTO department (id, name) VALUES (3, 'Legal');
+INSERT INTO department (id, name) VALUES (4, 'Finance');
+
+
+INSERT INT role (id, tittle, salary, department_id) VALUES (1, 'Software Engineer', 20000, 2)
+INSERT INT role (id, tittle, salary, department_id) VALUES (2, 'Sales Lead',19000, 1)
+INSERT INT role (id, tittle, salary, department_id) VALUES (3, 'Lead Engineer', 20000, 2)
+INSERT INT role (id, tittle, salary, department_id) VALUES (4, 'Sales person',50000 ,1)
+INSERT INT role (id, tittle, salary, department_id) VALUES (5, 'Accountant',15000 ,4)
+INSERT INT role (id, tittle, salary, department_id) VALUES (6, 'Legal team', 20000,3)
+INSERT INT role (id, tittle, salary, department_id) VALUES (7, ' lawyer', 19000,3)
+
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (1, 'Jasson', 'Sammon' , 1, null);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (2, 'Haseeb', 'Zubair' ,2 , 2);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (3, 'Waqas', 'Riaz' ,3 , null);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (4, 'John', 'Deacon' ,4 , null);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (5, 'Burton', 'Cliff' ,5 , 3);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (6, 'Robyn', 'Smith' ,6 , 1);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (7, 'Jordan', 'Hindby' ,7 , 2 );
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (8, 'Paul', 'Butterworth' ,8 , 3);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (9, 'Abdul', 'Sulieman' , 9, 6);
+
+
+
