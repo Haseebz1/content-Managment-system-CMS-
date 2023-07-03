db91372c0d078e002b79751137851e62a9f24ff4
Haseebz1 <haseebkandore@hotmail.com>
2023-07-03 01:03:40 +0100

    Created new instance of employedatabase class , and connected data base

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const inquirer = require('inquirer');
-const EmployeeDatabase = require('./db/')
+const= {MainMenuQuestions,AddDepartmentQuestions,AddRoleQuestions,AddEmployeeQuestions,UpdateEmployeeRoleQuestions };
+const EmployeeDatabase = require('./db/EmployeeDatabase');
+
+
+const db = new EmployeeDatabase({
+host: 'localhost',
+user: 'root',
+password: '',
+database: 'employee_db'
+});
+
+db.coonect();
