caa64f6f7314b1107740f21bb296d6582e9eac4e
Haseebz1 <haseebkandore@hotmail.com>
2023-07-05 00:51:44 +0100

    function for add department,roles,employees

--- index.js
+++ index.js
@@ -33,6 +33,57 @@ const view_employees = () => {
     });
 }
 
+const add_department = () => {
+    inquirer
+    .prompt(AddDepartmentsQuestions)
+    .then((response) => {
+        db.getdepartments(response).then((results) => {
+            console.log('\n', results, );
+            doMenuQuestions();
+        });
+
+    })
+   
+} 
+
+const add_role = () => {
+  db.getdepartments().then((results) => {
+    const departmentQuestions = AddRoleQuestions[2];
+    results.forEach((department) => {
+        departmentQuestions.choices.push({
+            value: department.id,
+            name: department.name,
+        });
+    });
+
+    inquirer 
+    .prompt(AddRoleQuestions)
+    .then((response) => {
+        db.addRole(response).then((results) => {
+            console.log('\n', results);
+            doMenuQuestions
+        });
+    });
+  });
+}
 
 
+const add_employee = () => {
+
+    db.getRoles().then((results) => {
+
+        const roleQuestions = AddEmployeeQuestions[2];
+        results.forEach((role) => {
+            const role_summary = `${role.tittle} (${role.department_name}: ${role.sa;ary})`;
+            roleQuestion.choices.push({
+                value: role.id,
+                name: role_summary
+            });   
+        });
+    
+    })
+
+
+
+    }
 
