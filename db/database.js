97f1ed239280117a5c3fb92d2cf408067cf2c7bf
Haseebz1 <haseebkandore@hotmail.com>
2023-07-04 17:20:31 +0100

    connection is made and validate

--- db/database.js
+++ db/database.js
@@ -14,5 +14,25 @@ class Database {
     return;
 }
 
+connect () {
+    this.validate();
+
+    const { host, user, password, database} = this.options;
+    this.db = mysql.createConnection(
+        {
+            host: host,
+            user: user,
+            password: password,
+            database: database
+        
+        },
+        console.log(`connecter to employee database,`)  
+    );
 }
+disconnect() {
+    this.db.disconnect();
+}
+}
+
+module.exports = Database;
 
