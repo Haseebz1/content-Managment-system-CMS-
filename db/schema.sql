787ea9e48586f1c3c8913c7f7b87a0b1b441609c
Haseebz1 <haseebkandore@hotmail.com>
2023-09-14 00:50:45 +0100

    Schema changed to try if it works

--- db/schema.sql
+++ db/schema.sql
@@ -1,31 +1,33 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
-USE employee_db;
+use employee_db;
 
 CREATE TABLE department
 (
-    id INT NOT NULL PRIMARY KEY,
-	name VARCHAR (30)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR (30) NOT NULL
 );
 
-CREATE TABLE roles
+CREATE TABLE role
 (
-    id INT NOT NULL PRIMARY KEY,
-    title VARCHAR (30),
-    salary INT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    FOREIGN KEY (department_id)
+     REFERENCES department(id)
+      ON DELETE SET NULL
 );
 
 CREATE TABLE employee 
 (
-    id INT NOT NULL PRIMARY KEY,
-    first_name VARCHAR (30),
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
     last_name VARCHAR (30),
-    roles_id INT,
+    role_id INT,
     manager_id INT,
-    FOREIGN KEY (roles_id)
-    REFERENCES roles (id)
+    FOREIGN KEY (role_id)
+    REFERENCES role (id)
     ON DELETE SET NULL
 );
