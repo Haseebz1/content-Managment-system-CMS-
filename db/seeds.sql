a4124e29bc0f4bf2b45e28e1dc5782a7250f5bbf
Haseebz1 <haseebkandore@hotmail.com>
2023-09-14 00:59:08 +0100

    seeds file changed layout pf insert into also removed id

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,31 @@
- INSERT INTO department (id, name) VALUES (1, 'Sales');
- INSERT INTO department (id, name) VALUES (2, 'Engineering');
- INSERT INTO department (id, name) VALUES (3, 'Legal');
- INSERT INTO department (id, name) VALUES (4, 'Finance');
-
-
-
- INSERT INTO roles (id, title, salary, department_id) VALUES (1, 'Software Engineer', 20000, 2);
- INSERT INTO roles (id, title, salary, department_id) VALUES (2, 'Sales Lead',19000, 1);
- INSERT INTO roles (id, title, salary, department_id) VALUES (3, 'Lead Engineer', 20000, 2);
- INSERT INTO roles (id, title, salary, department_id) VALUES (4, 'Sales person',50000 ,1);
- INSERT INTO roles (id, title, salary, department_id) VALUES (5, 'Accountant',15000 ,4);
- INSERT INTO roles (id, title, salary, department_id) VALUES (6, 'Legal team', 20000,3);
- INSERT INTO roles (id, title, salary, department_id) VALUES (7, ' lawyer', 1900, 4);
+ INSERT INTO department (name)
+  VALUES ("Sales"),
+   ("Engineering"),
+   ("Finance"),
+   ("Legal");
  
 
- INSERT INTO employee (id, first_name, last_name, roles_id, manager_id) VALUES (1, 'Jasson', 'Sammon', 1, null );
- INSERT INTO employee (id, first_name, last_name, roles_id, manager_id) VALUES (2, 'Haseeb', 'Zubair', 2 , 2 );
- INSERT INTO employee (id, first_name, last_name, roles_id, manager_id) VALUES (3, 'Waqas', 'Riaz', 3 , null);
- INSERT INTO employee (id, first_name, last_name, roles_id, manager_id) VALUES (4, 'John', 'Deacon', 4 , null);
- INSERT INTO employee (id, first_name, last_name, roles_id, manager_id) VALUES (5, 'Burton', 'Cliff', 5 , 3);
- INSERT INTO employee (id, first_name, last_name, roles_id, manager_id) VALUES (6, 'Robyn', 'Smith', 6 , 1);
- INSERT INTO employee (id, first_name, last_name, roles_id, manager_id) VALUES (7, 'Jordan', 'Hindby', 7 , 2 );
- INSERT INTO employee (id, first_name, last_name, roles_id, manager_id) VALUES (8, 'Paul', 'Butterworth', 8 , 3);
- INSERT INTO employee (id, first_name, last_name, roles_id, manager_id) VALUES (9, 'Abdul', 'Sulieman', 9, 6);
+
+
+ INSERT INTO role (title, salary, department_id) 
+ 
+VALUES  ('Software Engineer',3),
+('Sales Lead',19000, 1),
+('Lead Engineer', 20000, 2),
+('Sales person',50000 ,1),
+('Accountant',15000 ,4),
+('Legal team', 20000,3),
+(' lawyer', 1900, 4);
+ 
+
+ INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+ VALUES ('Jasson', 'Sammon', 1, null ),
+  ('Haseeb', 'Zubair', 2 , 2 ),
+  ('Waqas', 'Riaz', 3 , null),
+  ('John', 'Deacon', 4 , null),
+  ('Burton', 'Cliff', 5 , 3),
+  ('Robyn', 'Smith', 6 , 1),
+  ('Jordan', 'Hindby', 7 , 2 ),
+  ('Paul', 'Butterworth', 8 , 3),
+  ('Abdul', 'Sulieman', 9, 6);
 
