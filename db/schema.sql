beb0a417b58f4e09f29b8b0c049310b3eb2d1817
Haseebz1 <haseebkandore@hotmail.com>
2023-09-14 01:20:42 +0100

    Trying fixing this database

--- db/schema.sql
+++ db/schema.sql
@@ -1,33 +1,27 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
-
 use employee_db;
 
-CREATE TABLE department
-(
+CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	first_name VARCHAR (30) NOT NULL
+    departmentName VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role
-(
+CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR (30) NOT NULL,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id)
-     REFERENCES department(id)
-      ON DELETE SET NULL
+    FOREIGN KEY(department_id) 
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
-CREATE TABLE employee 
-(
+CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR (30) NOT NULL,
-    last_name VARCHAR (30),
+    first_name  VARCHAR(30) NOT NULL,
+    last_name  VARCHAR(30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id)
-    REFERENCES role (id)
+    FOREIGN KEY(role_id) 
+    REFERENCES role(id)
     ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,31 +1,24 @@
- INSERT INTO department (name)
-  VALUES ("Sales"),
-   ("Engineering"),
-   ("Finance"),
-   ("Legal");
- 
+INSERT INTO department(name)
+VALUES("Sales"),
+("Engineering"),
+("Finance"),
+("Legal");
 
+INSERT INTO role(title,salary,department_id)
+VALUES("Marketing lead",100000,1),
+("Marketing Junior", 50000,1),
+("Lead Engineer", 125000,2),
+("Junior Engineer",80000,2 ),
+("Accountant", 90000,3),
+("Legal Admin", 150000,4),
+("Lawyer", 185000,4);
 
-
- INSERT INTO role (title, salary, department_id) 
- 
-VALUES  ('Software Engineer',3),
-('Sales Lead',19000, 1),
-('Lead Engineer', 20000, 2),
-('Sales person',50000 ,1),
-('Accountant',15000 ,4),
-('Legal team', 20000,3),
-(' lawyer', 1900, 4);
- 
-
- INSERT INTO employee (first_name, last_name, role_id, manager_id) 
- VALUES ('Jasson', 'Sammon', 1, null ),
-  ('Haseeb', 'Zubair', 2 , 2 ),
-  ('Waqas', 'Riaz', 3 , null),
-  ('John', 'Deacon', 4 , null),
-  ('Burton', 'Cliff', 5 , 3),
-  ('Robyn', 'Smith', 6 , 1),
-  ('Jordan', 'Hindby', 7 , 2 ),
-  ('Paul', 'Butterworth', 8 , 3),
-  ('Abdul', 'Sulieman', 9, 6);
-
+INSERT INTO employee(first_name,last_name,role_id,manager_id)
+VALUES("Adam","Mills",3,null),
+("Jack",null,4,1),
+("John","Jones",1,null),
+("Zach","Roans",6,3),
+("Alex","Park",7,null),
+("Sam","Allan",2,2),
+("Jason","Doe",1,null),
+("Hick",null,5,null);
